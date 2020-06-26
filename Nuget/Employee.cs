61551661e7080ca0971a37d15c7c02ff185ad371
yoav-klein <yoavklein25@gmail.com>
2020-06-26 11:23:49 +0300

    Initial

---
+++ Nuget/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuget
+{
+    class Employee
+    {
+        public string Name;
+        public string Id;
+        public int Salary;
+        public string Position;
+    }
+}
---
+++ Nuget/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+
+namespace Nuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee bob = new Employee();
+            bob.Name = "Bob";
+            bob.Id = "300076247";
+            bob.Position = "Manager";
+            bob.Salary = 15000;
+
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(bob);
+            Console.WriteLine(json);
+        }
+    }
+}
